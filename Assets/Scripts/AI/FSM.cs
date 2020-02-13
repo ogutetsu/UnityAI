669d94e690b640621b8a8b626682d37e76d2e5ca
og <ogugu@test.com>
2020-02-13 11:09:21 +0900

    [add] FSMクラスを追加

---
+++ Assets/Scripts/AI/FSM.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FSM : MonoBehaviour
+{
+
+    protected Transform playerTransform;
+
+    protected Vector3 destPos;
+
+    protected GameObject[] pointList;
+
+    protected float shootRate;
+    protected float elapsedTime;
+
+    public Transform turret { get; set; }
+    public Transform bulletSpawnPoint { get; set; }
+
+    protected virtual void Initialize() { }
+    protected virtual void FSMUpdate() { }
+    protected virtual void FSMFixedUpdate() { }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FSMUpdate();
+    }
+
+    void FixedUpdate()
+    {
+        FSMFixedUpdate();
+    }
+
+}
