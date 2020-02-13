c0a61a8514e3c6b6c4a9006fbf9e1f12b509bd8c
og <ogugu@test.com>
2020-02-13 14:50:34 +0900

    [update] SlotMachineにペイテーブルリールストリップを実装

--- Assets/Scripts/Slot/SlotMachine.cs
+++ Assets/Scripts/Slot/SlotMachine.cs
@@ -26,6 +26,13 @@ public class SlotMachine : MonoBehaviour
     private int betAmount;
     private int credits = 1000;
 
+    public bool[] Weight = new bool[ReelNum];
+
+
+    private ArrayList weightedReelPoll = new ArrayList();
+    private int zeroProbability = 30;
+
+
     private int[] ReelResult = new int[ReelNum];
 
     private float elapsedTime = 0.0f;
@@ -35,7 +42,21 @@ public class SlotMachine : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < zeroProbability; i++)
+        {
+            weightedReelPoll.Add(0);
+        }
+
+        int reminingValuesProb = (100 - zeroProbability) / 9;
+        for (int j = 1; j < 10; j++)
+        {
+            for (int k = 0; k < reminingValuesProb; k++)
+            {
+                weightedReelPoll.Add(j);
+            }
+        }
+
+
     }
 
     // Update is called once per frame
@@ -74,20 +95,34 @@ public class SlotMachine : MonoBehaviour
 
     void checkBet()
     {
-        bool Win = true;
+        bool jackpot = true;
         for (int i = 0; i < ReelNum-1; i++)
         {
             if (ReelResult[i] != ReelResult[i + 1])
             {
-                Win = false;
+                jackpot = false;
             }
 
         }
 
-        if (Win)
+        if (jackpot)
         {
-            betResult.text = "YOU WIN!";
-            credits += 500 * betAmount;
+            betResult.text = "JACKPOT!";
+            credits += 50 * betAmount;
+        }
+        else if (ReelResult[0] == 0 && ReelResult[2] == 0)
+        {
+            betResult.text = "YOU WIN" + (betAmount/2).ToString();
+            credits -= (betAmount / 2);
+        }
+        else if (ReelResult[0] == ReelResult[1])
+        {
+            betResult.text = "AWW... ALMOST JACKPOT!";
+        }
+        else if (ReelResult[0] == ReelResult[2])
+        {
+            betResult.text = "YOU WIN" + (betAmount * 2).ToString();
+            credits -= (betAmount * 2);
         }
         else
         {
@@ -107,15 +142,39 @@ public class SlotMachine : MonoBehaviour
             {
                 if (!ReelSpinned[i])
                 {
+
                     ReelText[i].text = randomSpinResult.ToString();
                     if (elapsedTime >= spinDuration)
                     {
-                        ReelResult[i] = randomSpinResult;
+                        int result = randomSpinResult;
+                        if (Weight[i])
+                        {
+                            int weightedRandom = Random.Range(0, weightedReelPoll.Count);
+                            result = (int) weightedReelPoll[weightedRandom];
+                        }
+
+                        ReelResult[i] = result;
+                        ReelText[i].text = result.ToString();
                         ReelSpinned[i] = true;
                         elapsedTime = 0;
 
+                        //もう少しスマートにするならコールバックを用意する
+                        //最後のリール
                         if (i == ReelNum - 1)
                         {
+                            if ((ReelResult[0] == ReelResult[1]) &&
+                                randomSpinResult != ReelResult[0])
+                            {
+                                randomSpinResult = ReelResult[0] - 1;
+                                if (randomSpinResult < ReelResult[0]) randomSpinResult = ReelResult[0] - 1;
+                                if (randomSpinResult > ReelResult[0]) randomSpinResult = ReelResult[0] + 1;
+                                if (randomSpinResult < 0) randomSpinResult = 0;
+                                if (randomSpinResult > 9) randomSpinResult = 9;
+
+                                ReelText[i].text = randomSpinResult.ToString();
+                                ReelResult[i] = randomSpinResult;
+                            }
+                            
                             startSpin = false;
                             for (int j = 0; j < ReelNum; j++)
                             {
