82b3a8b3d95b37d5144a0fd78344f5fbf6a3d9d4
og <ogugu@test.com>
2020-02-14 12:22:54 +0900

    [add] NavTargetクラスを追加
    マウスをクリックするとNavターゲットをその位置へ移動させ
    NavAgentをターゲットに移動させます。

---
+++ Assets/Scripts/NavMesh/NavTarget.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavTarget : MonoBehaviour
+{
+
+    private NavMeshAgent[] navAgents;
+    public Transform targetMarker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navAgents = FindObjectsOfType<NavMeshAgent>();
+    }
+
+    void UpdateTargets(Vector3 targetPosition)
+    {
+        foreach (NavMeshAgent agent in navAgents)
+        {
+            agent.destination = targetPosition;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int button = 0;
+
+        if (Input.GetMouseButtonDown(button))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                Vector3 targetPosition = hitInfo.point;
+                UpdateTargets(targetPosition);
+                targetMarker.position = targetPosition + new Vector3(0, 5, 0);
+            }
+
+        }
+
+    }
+}
